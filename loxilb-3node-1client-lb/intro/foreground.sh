7ac3244cdf85a0984a32a52b3b9b8320b47eda99
seokhwan.kong@netlox.io <seokhwan.kong@netlox.io>
2022-08-17 16:32:35 +0900

    change background shell to fronground shell

--- loxilb-3node-1client-lb/intro/foreground.sh
+++ loxilb-3node-1client-lb/intro/foreground.sh
@@ -1,3 +1,74 @@
-echo waiting for init-background-script to finish
-while [ ! -f /tmp/background0 ]; do sleep 1; done
+set -x # to test stderr output in /var/log/killercoda
+echo starting... # to test stdout output in /var/log/killercoda
+
+echo '============= Make 3 Endpoints, 1 Client and 1 LoxiLB host ============'
+
+docker=$1
+HADD="sudo ip netns add "
+LBHCMD="sudo ip netns exec loxilb "
+HCMD="sudo ip netns exec "
+
+id=`docker ps -f name=loxilb | cut  -d " "  -f 1 | grep -iv  "CONTAINER"`
+echo $id
+pid=`docker inspect -f '{{.State.Pid}}' $id`
+if [ ! -f /var/run/netns/loxilb ]; then
+  sudo touch /var/run/netns/loxilb
+  sudo mount -o bind /proc/$pid/ns/net /var/run/netns/loxilb
+fi
+
+$HADD l3e1
+$HADD l3e2
+$HADD l3e3
+$HADD l3c1
+
+
+echo '============= Configure load-balancer end-point l3e1 ============'
+sudo ip -n loxilb link add enp1 type veth peer name eth0 netns l3e1
+sudo ip -n loxilb link set enp1 mtu 9000 up
+sudo ip -n l3e1 link set eth0 mtu 7000 up
+$LBHCMD ip addr add 31.31.31.254/24 dev enp1
+$HCMD l3e1 ifconfig eth0 31.31.31.1/24 up
+$HCMD l3e1 ip route add default via 31.31.31.254
+$HCMD l3e1 lo up
+
+
+echo '============= Configure load-balancer end-point l3e2 ============'
+sudo ip -n loxilb link add enp2 type veth peer name eth0 netns l3e2
+sudo ip -n loxilb link set enp2 mtu 9000 up
+sudo ip -n l3e2 link set eth0 mtu 7000 up
+$LBHCMD ip addr add 32.32.32.254/24 dev enp2
+$HCMD l3e2 ifconfig eth0 32.32.32.2/24 up
+$HCMD l3e2 ip route add default via 32.32.32.254
+$HCMD l3e2 lo up
+
+echo '============= Configure load-balancer end-point l3e3 ============'
+sudo ip -n loxilb link add enp3 type veth peer name eth0 netns l3e3
+sudo ip -n loxilb link set enp3 mtu 9000 up
+sudo ip -n l3e3 link set eth0 mtu 7000 up
+$LBHCMD ip addr add 17.17.17.254/24 dev enp3
+$HCMD l3e3 ifconfig eth0 17.17.17.1/24 up
+$HCMD l3e3 ip route add default via 17.17.17.254
+$HCMD l3e3 lo up
+
+echo '============= Configure load-balancer end-point l3c1 ============'
+sudo ip -n loxilb link add enp4 type veth peer name eth0 netns l3c1
+sudo ip -n loxilb link set enp4 mtu 9000 up
+sudo ip -n l3c1 link set eth0 mtu 7000 up
+$LBHCMD ip addr add 100.100.100.254/24 dev enp4
+$HCMD l3c1 ifconfig eth0 100.100.100.1/24 up
+$HCMD l3c1 ip route add default via 100.100.100.254
+$HCMD l3c1 lo up
+
+echo '============= install wrk(http traffic generator) Tool ============'
+sudo apt-get install build-essential libssl-dev git -y 
+git clone https://github.com/wg/wrk.git wrk 
+cd wrk 
+sudo make 
+sudo cp wrk /usr/local/bin 
+cd ../
+
+echo '============= install http echo server ============'
+cd ~/
+go build server.go
+
 echo Hello and welcome to LoxiLB sample scenario!
