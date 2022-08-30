62833c7f41484e5c57114cd3317c5fd51eebd750
seokhwan.kong@netlox.io <seokhwan.kong@netlox.io>
2022-08-30 20:59:54 +0900

    commit

--- loxilb-ccm/intro/foreground.sh
+++ loxilb-ccm/intro/foreground.sh
@@ -1,4 +1,16 @@
 echo waiting for init-background-script to finish
 echo ====== It will tabke 5 - 10 minutes ======
-while [ ! -f /tmp/background0 ]; do sleep 1; done
+sudo su root
+apt install -y clang llvm libelf-dev gcc-multilib libpcap-dev vim net-tools linux-tools-$(uname -r) elfutils dwarves git libbsd-dev bridge-utils wget unzip build-essential bison flex iproute2
+git clone https://github.com/loxilb-io/iproute2.git
+cd iproute2
+cd libbpf/src/
+mkdir build
+DESTDIR=build make install
+cd ../../
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:`pwd`/libbpf/src/
+LIBBPF_FORCE=on LIBBPF_DIR=`pwd`/libbpf/src/build ./configure
+make
+sudo cp -f tc/tc /usr/local/sbin/ntc
+cd ~
 echo Hello and welcome to this LoxiLB scenario!
