5bef478210071ab51d59be3aae1391dbab338be4
NLX-SeokHwanKong <seokhwan.kong@netlox.io>
2022-12-28 07:24:09 +0900

    add wireshark server for test

--- loxilb-tcplb/intro/background.sh
+++ loxilb-tcplb/intro/background.sh
@@ -6,6 +6,19 @@ echo '============= Make 3 Endpoints, 1 Client and 1 LoxiLB ============'
 apt install -y net-tools socat iperf nodejs netcat libsctp-dev jq
 sleep 3
 
+echo '============= Install Wireshark Server for Analytics ============'
+docker run -d \
+  --name=wireshark \
+  --net=host \
+  --cap-add=NET_ADMIN \
+  --security-opt seccomp=unconfined `#optional` \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -p 3000:3000 `#optional` \
+  --restart unless-stopped \
+  lscr.io/linuxserver/wireshark:latest
+
 cd ~/
 sudo /bin/bash ./start.sh
 
